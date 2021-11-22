ac2eaf487caf64fcc45dd79eae216ff231fc831b
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2021-11-22 06:04:10 +0100

    deleted test

--- Tests/TVShowsEndpointTests/TVShowsEndpointTests.swift
+++ Tests/TVShowsEndpointTests/TVShowsEndpointTests.swift
@@ -6,6 +6,6 @@ final class TVShowsEndpointTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(TVShowsEndpoint().text, "Hello, World!")
+//        XCTAssertEqual(TVShowsEndpoint().text, "Hello, World!")
     }
 }
